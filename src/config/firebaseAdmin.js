d2adbb5f2d15f777cdaee9673ac77a6ffe4ca60d
MAHAK0804 <2021pietcsmahak100@poornima.org>
2025-06-06 14:21:13 +0530

    chat backend

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -3,13 +3,11 @@ import admin from "firebase-admin";
 
 dotenv.config();
 
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
